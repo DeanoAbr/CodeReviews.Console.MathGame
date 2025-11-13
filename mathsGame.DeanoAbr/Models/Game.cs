a7670ed4361c5f5b7b74145638ff57787a76d6e2
DeanoAbr <dean.tyrell.cranko@gmail.com>
2025-11-13 19:54:54 +0200

    Clean up Game class by removing unused code
    Removed commented-out score property and backing field.

--- mathsGame.DeanoAbr/Models/Game.cs
+++ mathsGame.DeanoAbr/Models/Game.cs
@@ -2,13 +2,6 @@ namespace Academy.Console.MathGame.Models;
 
 internal class Game
 {
-	//private int _score;
-
-	//public int Score 
-	//{ 
-	//    get { return _score; } 
-	//    set { _score = value; }
-	//}
 	public DateTime Date { get; set; }
 
 	public int Score { get; set; }
